bc7d68e626aae0e33a8c64026515e808d40a0f2d
George Jones <gmj@pobox.com>
2016-01-28 05:47:01 -0500

    Added gmj-org-eww-store-link.

---
+++ gmj-org-eww-store-link.el
@@ -0,0 +1,29 @@
+(add-hook 'org-store-link-functions 'org-eww-store-link)
+
+(defun gmj-eww-copy-page-title ()
+  "Copy the title of the current page into the kill ring."
+  (interactive)
+  (message "%s" (plist-get eww-data :title))
+  (kill-new (plist-get eww-data :title)))
+
+
+(defun gmj-org-eww-store-link ()
+  "Store a link to a eww buffer."
+  (when (eq major-mode 'eww-mode)
+    (org-store-link-props
+     :type "eww"
+     :link (eww-copy-page-url)
+     :url (url-view-url t)
+     :description (or (gmj-eww-copy-page-title) (eww-copy-page-url)))))
+
+
+
+
+
+
+
+
+
+
+
+
