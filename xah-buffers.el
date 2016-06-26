86176bd79635b9c0a07454232d6b87f665d9e108
George Jones <gmj@pobox.com>
2016-06-26 07:39:25 -0400

    Add C-f[11,12] to cycle GNUS buffers

--- xah-buffers.el
+++ xah-buffers.el
@@ -71,3 +71,60 @@ Version 2016-06-19"
 (global-set-key (kbd "<S-f11>") 'xah-previous-emacs-buffer)
 (global-set-key (kbd "<S-f12>") 'xah-next-emacs-buffer)
 
+; Add nex/previous gnus buffer functions and bindings
+
+(defun gmj-next-gnus-buffer ()
+  "Switch to the next gnus buffer.
+“gnus buffer” is determined by `gmj-gnus-buffer-q'.
+adapted from URL `http://ergoemacs.org/emacs/elisp_next_prev_gnus_buffer.html'
+Version 2016-06-19"
+  (interactive)
+  (next-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (not (gmj-gnus-buffer-q))
+          (progn (next-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+(defun gmj-previous-gnus-buffer ()
+  "Switch to the previous gnus buffer.
+“gnus buffer” is determined by `gmj-gnus-buffer-q'.
+adapted from URL `http://ergoemacs.org/emacs/elisp_next_prev_gnus_buffer.html'
+Version 2016-06-19"
+  (interactive)
+  (previous-buffer)
+  (let ((i 0))
+    (while (< i 20)
+      (if (not (gmj-gnus-buffer-q))
+          (progn (previous-buffer)
+                 (setq i (1+ i)))
+        (progn (setq i 100))))))
+
+(defun gmj-gnus-buffer-q ()
+  "Return t if current buffer is a user buffer, else nil.
+Typically, if buffer name starts with *, it's not considered a user buffer.
+This function is used by buffer switching command and close buffer command, so that next buffer shown is a user buffer.
+You can override this function to get your idea of “user buffer”.
+version 2016-06-18"
+  (interactive)
+  (if (string-equal (substring (symbol-name major-mode) 0 4) "gnus")
+      t
+    nil))
+
+(global-set-key (kbd "<C-f11>") 'gmj-previous-gnus-buffer)
+(global-set-key (kbd "<C-f12>") 'gmj-next-gnus-buffer)
+
+; Tips on keeping the # of buffers reasonable:
+;                                       ;
+;   http://irreal.org/blog/?p=765
+;   http://ergoemacs.org/emacs_manual/emacs/Kill-Buffer.html  
+; 
+;   Use
+;
+;     M-X clean-buffer-list
+;                                       ;
+;   to automatically clean up unused buffers.
+;
+;   Also use ibuffer to mark unwanted buffers, then type "D"                                        ;
+
