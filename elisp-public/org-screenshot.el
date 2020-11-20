50edda6cd8cf7e37ad2458a7070ecc9730c38d37
George Jones <gmj@pobox.com>
2020-11-19 19:33:30 -0500

    .gitignore

--- elisp-public/org-screenshot.el
+++ elisp-public/org-screenshot.el
@@ -1,42 +1,42 @@
 ;; https://www.reddit.com/r/emacs/comments/jlynfq/take_screenshot_of_region_on_linux_for_org_mode/
 
-(defun my-insert-clipboard-png ()  
-      (interactive)  
-      (let  
-          ;; Read Filename from Minibuffer  
-          ((filename (read-from-minibuffer "image file name: "))  
-           (directory "_media"))  
+;; (defun my-insert-clipboard-png ()  
+;;       (interactive)  
+;;       (let  
+;;           ;; Read Filename from Minibuffer  
+;;           ((filename (read-from-minibuffer "image file name: "))  
+;;            (directory "_media"))  
        
-        ;; Use maim to screenshot  
-        (shell-command (format "mkdir -p %s/%s" default-directory directory))  
-        (shell-command (format "xclip -selection clipboard -t image/png -o > %s/%s/%s.png" default-directory directory  
-                               filename ))  
+;;         ;; Use maim to screenshot  
+;;         (shell-command (format "mkdir -p %s/%s" default-directory directory))  
+;;         (shell-command (format "xclip -selection clipboard -t image/png -o > %s/%s/%s.png" default-directory directory  
+;;                                filename ))  
         
-        ;; Insert formatted link at point  
-        (save-excursion (insert(format  
-                                "#+attr_html: :width 400px \n #+attr_latex: :width 0.4\\textwidth \n [[file:%s/%s.png]]"  
-                                directory filename)))  
+;;         ;; Insert formatted link at point  
+;;         (save-excursion (insert(format  
+;;                                 "#+attr_html: :width 400px \n #+attr_latex: :width 0.4\\textwidth \n [[file:%s/%s.png]]"  
+;;                                 directory filename)))  
         
-        ;; Message success to the minibuffer  
-        (message "saved to %s as %s.png" directory filename)  
-        )  
-      )
+;;         ;; Message success to the minibuffer  
+;;         (message "saved to %s as %s.png" directory filename)  
+;;         )  
+;;       )
 
-    (defun my-take-screenshot ()  
-      (interactive)  
-      (let  
-          ;; Read Filename from Minibuffer  
-          ((filename (read-from-minibuffer "image file name: "))  
-           (directory "_media"))  
+;;     (defun my-take-screenshot ()  
+;;       (interactive)  
+;;       (let  
+;;           ;; Read Filename from Minibuffer  
+;;           ((filename (read-from-minibuffer "image file name: "))  
+;;            (directory "_media"))  
         
-        ;; Use maim to screenshot  
-        (shell-command (format "maim --select %s/%s/%s.png" default-directory directory filename ))  
+;;         ;; Use maim to screenshot  
+;;         (shell-command (format "maim --select %s/%s/%s.png" default-directory directory filename ))  
         
-        ;; Insert formatted link at point  
-        (save-excursion (insert(format  
-                                "#+attr_html: :width 400px \n #+attr_latex: :width 0.4\\textwidth \n [[file:%s/%s.png]]"  
-                                directory filename)))  
+;;         ;; Insert formatted link at point  
+;;         (save-excursion (insert(format  
+;;                                 "#+attr_html: :width 400px \n #+attr_latex: :width 0.4\\textwidth \n [[file:%s/%s.png]]"  
+;;                                 directory filename)))  
         
-        ;; Message success to the minibuffer  
-        (message "saved to %s as %s.png" directory filename)  
-        )
+;;         ;; Message success to the minibuffer  
+;;         (message "saved to %s as %s.png" directory filename)  
+;;         )
